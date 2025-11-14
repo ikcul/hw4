b320e52e7099517e16ead564b07fb70156e995f1
Daniel <simdaniel06@gmail.com>
2025-11-14 11:43:56 -0800

    bst

--- bst.h
+++ bst.h
@@ -247,7 +247,7 @@ protected:
     virtual void nodeSwap( Node<Key,Value>* n1, Node<Key,Value>* n2) ;
 
     // Add helper functions here
-    int isBalancedHelper(const Node<Key, Value> *root);
+    int isBalancedHelper(const Node<Key, Value> *root) const;
 
 
 protected:
